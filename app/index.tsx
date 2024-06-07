eff3309c278193211a4cd56da42f99b78f9bf2d4
Akshat Dubey <akshatdubey0808@gmail.com>
2024-06-08 00:44:27 +0530

    initial setup and tabs navigation

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <Link className="text-blue-600" href="/home">
+        Home
+      </Link>
+    </View>
+  );
+}
